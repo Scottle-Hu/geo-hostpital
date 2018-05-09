5cd05a390efd9ae4480751c1df2858c304f43cfd
huqj <huqj@rd.youdao.com>
2018-05-09 12:25:21 +0800

    修改数据库密码

--- src/main/java/com/charles/geo/utils/DBUtil.java
+++ src/main/java/com/charles/geo/utils/DBUtil.java
@@ -19,7 +19,7 @@ public class DBUtil {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection("jdbc:mysql://122.114.206.52:3306/db_geo?characterEncoding=utf8&autoReconnect=true"
-                    , "ssgxhn$$$2018", "123456");
+                    , "root", "ssgxhn$$$2018");
             return connection;
         } catch (Exception e) {
             e.printStackTrace();
